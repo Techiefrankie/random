8b570681b0939bea1000572aa50037a942132030
Franklyn Ogbonna <techiefrankie@gmail.com>
2021-06-23 01:54:22 +0100

    Update ArrayRotation.java

--- src/com/techie/alogorithms/arrayrotation/ArrayRotation.java
+++ src/com/techie/alogorithms/arrayrotation/ArrayRotation.java
@@ -1,6 +1,7 @@
 package com.techie.alogorithms.arrayrotation;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ArrayRotation {
@@ -33,8 +34,46 @@ public class ArrayRotation {
         return Arrays.asList(rotatedArray);
     }
 
+    static List<Integer> rotateLeft1(List<Integer> a, int d) {
+        Integer[] rotatedArray = a.toArray(new Integer[a.size()]);
+        for (int s = d, t = 0; t < a.size(); s++, t++) {
+            int j = t + 1; // to make up for array index starting at 0
+            int newIndex = t - 1 < 0 ? rotatedArray.length - j : t - 1;
+            rotatedArray[newIndex] = a.get(t);
+        }
+        return Arrays.asList(rotatedArray);
+    }
+
     public static void main(String[] args){
         List<Integer> array = Arrays.asList(1, 2, 3, 4, 5);
         System.out.println(rotateLeft(array, 4));
+    }
+
+    public static List<Integer> encryptionValidity(int instructionCount, int validityPeriod, List<Integer> keys) {
+        // Write your code here
+        //convert the keys list param to an array for easier array indexing operations
+        Integer[] encrytionValidity  = keys.toArray(new Integer[keys.size()]);
+
+        //compute the number of times a hacker could try
+        int retries = instructionCount * validityPeriod;
+
+        // create an array to hold the degrees of divisibility of the elements
+        Integer[] divisibility = new Integer[encrytionValidity.length];
+
+        // iterate through the keys array and compute the divisibility degrees
+
+        for(int i = 0; i < encrytionValidity.length; i++){
+            //create an inner loop to for inner comparison with other elements
+            for (int j = 0; j < encrytionValidity.length; j++){
+                if(encrytionValidity[i] % encrytionValidity[j] == 0){
+                    //elements are divisible, increment count
+                    counter++;
+                }
+            }
+        }
+
+        int max = Collections.max(Arrays.asList(divisibility));
+        int ec = (int) (max *  Math.pow(2,3));
+        return Arrays.asList(1, ec);
     }
 }
